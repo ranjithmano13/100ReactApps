d58e580fc07079c3e1efb99ca3a5da65b0392651
ranjithmano13 <ranjithmano13@gmail.com>
2023-06-09 22:20:42 +0530

    home page card design completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,137 @@
 import './App.css';
+import ProjectContainer from './ProjectContainer';
+
+const projects = [
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+  {
+    name:'Movie Search',
+    description:'A simple movie search react app connecting with OMDB API',
+    src:'images/simple-movie-search.png',
+    giturl:'https://github.com/ranjithmano13/simple-movie-search-react-app.git',
+    projectLink:'https://ranjithmano13.github.io/simple-movie-search-react-app'
+  },
+]
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className='header'>100 React Apps</h1>
+      <ul>
+      {projects.map((project)=>(
+        <ProjectContainer name={project.name} description={project.description} src={project.src} giturl={project.giturl} projectLink={project.projectLink}/>
+      ))}
+      </ul>
     </div>
   );
 }
---
+++ src/ProjectContainer.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export default function ProjectContainer(props) {
+  const [pstyle, setPstyle] = useState({
+    display: "none",
+  });
+  const showP = () => {
+    setPstyle({
+      display: "block",
+      backdropFilter: "blur(15px)",
+      animation: "slide-in 0.3s  forwards alternate",
+    });
+    
+  };
+
+  const hideP = () =>
+    setPstyle({
+      display: "none",
+    });
+  return (
+    <li>
+      <article className="app-card" onMouseEnter={showP} onMouseLeave={hideP}>
+        <header>
+          <img src={props.src} alt={props.name} />
+          <h1>{props.name}</h1>
+        </header>
+        <main>
+          <p style={pstyle}>{props.description} </p>
+        </main>
+        <div>
+          <a
+            href={props.giturl}
+            target="_blank"
+          >
+            <i className="fa-brands fa-github fa-lg"></i>
+          </a>
+          <a
+            href={props.projectLink}
+            target="_blank"
+          >
+            <i className="fa-solid fa-arrow-up-right-from-square fa-lg" tootip="live"></i>
+          </a>
+        </div>
+      </article>
+    </li>
+  );
+}
